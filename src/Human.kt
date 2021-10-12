3b635806823bfa95509768b972f4f948320ca7c5
andosinanyan <62666310+andosinanyan@users.noreply.github.com>
2021-10-12 21:27:13 +0400

    This is our first gir repo

---
+++ src/Human.kt
@@ -0,0 +1,10 @@
+class Human(var name : String = "Arpine", var surname: String, var age: Int) {  //model of the object
+        init{
+            println("$name")
+            age + 18 / 21
+        }
+
+    companion object{
+        const val aaa = 15
+    }
+}
---
+++ src/Test.java
@@ -0,0 +1,10 @@
+public class Test {
+    public static void main(String[] args) {
+        int a = 15;
+        float b= 15.36f;
+        String bc = "Text";
+        boolean po = true;
+        char bqw = 'q';
+
+    }
+}
